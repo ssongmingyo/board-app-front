873849edbdd16e4c0f6a88ada731671dcfd87167
ssongmingyo <alsrysong@gmail.com>
2024-09-27 16:46:59 +0900

    CI/CD test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Home = () => {
+  useEffect(() => {
+    alert("안녕하세요.");
+  }, []);
+
   return (
     <div>게시판 홈</div>
   );
